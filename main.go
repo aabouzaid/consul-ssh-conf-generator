d58f226ca87b7758b3b2654bb5ac313b9f16c09a
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2018-07-26 00:34:57 +0200

    add functions to make binary works as cmd

--- main.go
+++ main.go
@@ -6,15 +6,32 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
 )
 
-var (
-	host, port = consul2ssh.GetEnvKey("LISTEN_HOST", ""), consul2ssh.GetEnvKey("LISTEN_PORT", "8001")
-	listenAddr = fmt.Sprintf("%s:%s", host, port)
-)
-
-func main() {
+func api() {
+	var (
+		host, port = consul2ssh.GetEnvKey("LISTEN_HOST", ""), consul2ssh.GetEnvKey("LISTEN_PORT", "8001")
+		listenAddr = fmt.Sprintf("%s:%s", host, port)
+	)
 	router := mux.NewRouter()
 	router.HandleFunc("/nodes", consul2ssh.GetNodes).Methods("GET")
 	log.Fatal(http.ListenAndServe(listenAddr, router))
+}
+
+func cmd() {
+	consul2ssh.GetNodesCMD(os.Args[2:])
+}
+
+func main() {
+	// Work as command line.
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "get":
+			cmd()
+		}
+	// Work as API.
+	} else {
+		api()
+	}
 }
