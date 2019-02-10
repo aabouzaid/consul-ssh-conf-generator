937a764a07f0d12471cfce669ac106ae5fc36176
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2019-02-10 13:33:07 +0100

    test: cover iterable items

--- consul2ssh/templating_test.go
+++ consul2ssh/templating_test.go
@@ -3,6 +3,7 @@ package consul2ssh
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -11,6 +12,10 @@ func TestFmtSSHElems(t *testing.T) {
 		"key01": "val",
 		"key02": true,
 		"key03": 1,
+		"key04": []interface{}{
+			"subkey4.1",
+			"subkey4.2",
+		},
 	}
 	output := fmtSSHElems(input)
 
@@ -19,7 +24,21 @@ func TestFmtSSHElems(t *testing.T) {
 	inputLen := make([]int, len(input))
 	for keyID := range inputLen {
 		key := fmt.Sprintf("key%02d", keyID+1)
-		elemKeyValue := fmt.Sprintf("%v %v", key, input[key])
-		assert.Equal(t, elemKeyValue, output[keyID])
+		value := input[key]
+		rt := reflect.TypeOf(value)
+
+		switch rt.Kind() {
+		// If the element is iterable we use the same main key but with element value.
+		case reflect.Slice, reflect.Array:
+			valueList := value.([]interface{})
+			for subID, item := range valueList {
+				elemKeyValue := fmt.Sprintf("%v %v", key, item)
+				assert.Equal(t, elemKeyValue, output[keyID+subID])
+			}
+		// Non-iterable elements.
+		default:
+			elemKeyValue := fmt.Sprintf("%v %v", key, value)
+			assert.Equal(t, elemKeyValue, output[keyID])
+		}
 	}
 }
