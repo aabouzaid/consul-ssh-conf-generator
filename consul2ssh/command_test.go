029b696cbe3581824abdca2bdc629de462758df1
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2019-02-10 22:13:02 +0100

    test: add unit test for the app as cmd

--- consul2ssh/command_test.go
+++ consul2ssh/command_test.go
@@ -1,9 +1,15 @@
 package consul2ssh
 
 import (
-	"os"
-	"testing"
+	"fmt"
+	"github.com/kami-zh/go-capturer"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
 )
 
 func TestReadConfFile(t *testing.T) {
@@ -39,4 +45,69 @@ func TestReadConfFile(t *testing.T) {
 			t.Errorf("%v: got %q, want %q.", tp.message, fileContent, tp.expected)
 		}
 	}
+}
+
+func TestGetNodesCMD(t *testing.T) {
+
+	// CMD side.
+	config := []byte(`
+		  {
+		    "api": {
+		    },
+		    "main": {
+		      "prefix": "dv",
+		      "jumphost": "bastion01.fqdn",
+		      "domain": "consul"
+		    },
+		    "global": {
+		      "User": "foo",
+		      "Port": 22
+		    },
+		    "pernode": {
+		      "bastion01": {
+		        "ForwardAgent": "yes"
+		      }
+		    },
+		    "custom": {
+		      "cassandra-local-proxy": {
+		        "TCPKeepAlive": "yes",
+		        "LocalForward": [
+		          "9042 node02:9042"
+		        ]
+		      }
+		    }
+		  }
+	`)
+
+	// TODO: Find a better way rather than creating tmpfile.
+	tmpfile, err := ioutil.TempFile("", "config.json")
+	checkErrCMD(err)
+	// Clean up later.
+	defer os.Remove(tmpfile.Name())
+	// Write content.
+	if _, err := tmpfile.Write(config); err != nil {
+		tmpfile.Close()
+		checkErrCMD(err)
+	}
+
+	// API side.
+	expected := "dummy data"
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, expected)
+	}))
+	defer server.Close()
+
+	// Run GetNodesCMD function and read from stdout.
+	args := []string{
+		"-config-file", tmpfile.Name(),
+		"-url", server.URL + c2sNodesEndpoint,
+	}
+
+	actual := capturer.CaptureStdout(func() {
+		GetNodesCMD(args)
+	})
+
+	assert.Equal(t, expected, strings.TrimSpace(actual))
+
 }
